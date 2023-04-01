1dadc01d8561cfe365001ea0e2841022d9d60508
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2023-04-02 02:58:33 +0300

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -1,75 +1,57 @@
+import tkinter as tk
 import wmi
 import platform
 import socket
 import datetime
+import pyperclip
 
-# create a WMI object to access the system information
 wmi_obj = wmi.WMI()
-
-# retrieve the first instance of the Win32_BaseBoard class
 baseboard = wmi_obj.Win32_BaseBoard()[0]
-
-# retrieve the first instance of the Win32_ComputerSystem class
 computer_system = wmi_obj.Win32_ComputerSystem()[0]
-
-# retrieve the first instance of the Win32_SystemEnclosure class
 system_enclosure = wmi_obj.Win32_SystemEnclosure()[0]
-
-# retrieve the first instance of the Win32_OperatingSystem class
 operating_system = wmi_obj.Win32_OperatingSystem()[0]
-
-# Get the local IP address
 ip_address = socket.gethostbyname(socket.gethostname())
-
-# Get the computer hostname
 hostname = platform.node()
-
-# retrieve the serial number of the baseboard
 serial_number = baseboard.SerialNumber.strip()
-
-# retrieve the value of the UserName property
 user_name = computer_system.UserName
 domain =   computer_system.Domain 
 model_name = computer_system.SystemFamily
 model_no =   computer_system.Model
-
-# retrieve the value of the SerialNumber property
 system_serial_number = system_enclosure.SerialNumber.strip()
-
-# retrieve the value of the InstallDate property
 install_date_string = operating_system.InstallDate
-
-# convert the InstallDate string to a datetime object
 install_date = datetime.datetime.strptime(install_date_string.split(".")[0], "%Y%m%d%H%M%S")
 
-# Display the information in a BGInfo-like format
-print("IP Address: " + ip_address)
-print("Hostname: " + hostname)
-print(f"Baseboard Serial Number: {serial_number}")
-print(f"User Name: {user_name}")
-print(f"Computer Domain: {domain}")
-print(f"Model Name: {model_name}")
-print(f"Model No: {model_no}")
-print(f"Sytem Serial Number: {system_serial_number}")
-print(f"Windows Installation Date: {install_date}")
+# Create a new window
+root = tk.Tk()
+root.title("BT DESTEK")
+
+#center window
+width = 420 # Width 
+height = 290 # Height
+
+screen_width = root.winfo_screenwidth()  # Width of the screen
+screen_height = root.winfo_screenheight() # Height of the screen
+ 
+# Calculate Starting X and Y coordinates for Window
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+ 
+root.geometry(
+'%dx%d+%d+%d' % (width, height, x, y))
+root.resizable(0, 0)
 
 
+# Add labels to display the system information
+tk.Label(root, text="IP Address: " + ip_address, anchor='w').pack(fill='x')
+tk.Label(root, text="Hostname: " + hostname, anchor='w').pack(fill='x')
+tk.Label(root, text=f"Baseboard Serial Number: {serial_number}", anchor='w').pack(fill='x')
+tk.Label(root, text=f"User Name: {user_name}", anchor='w').pack(fill='x')
+tk.Label(root, text=f"Computer Domain: {domain}", anchor='w').pack(fill='x')
+tk.Label(root, text=f"Model Name: {model_name}", anchor='w').pack(fill='x')
+tk.Label(root, text=f"Model No: {model_no}", anchor='w').pack(fill='x')
+tk.Label(root, text=f"System Serial Number: {system_serial_number}", anchor='w').pack(fill='x')
+tk.Label(root, text=f"Windows Installation Date: {install_date}", anchor='w').pack(fill='x')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Start the GUI event loop
+root.mainloop()
 
